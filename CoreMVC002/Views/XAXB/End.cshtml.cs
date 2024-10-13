1b6a85843487436326975950ee24f89b473bf5e9
Kevin <Kevin@MSI-9299>
2024-10-14 00:53:43 +0800

    加入專案檔案。

---
+++ CoreMVC002/Views/XAXB/End.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreMVC002.Views.XAXB
+{
+    public class EndModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
